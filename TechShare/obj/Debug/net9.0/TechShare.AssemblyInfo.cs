eb6a4ab93cf81fa42c78b7296ca2048ba976b93e
ToanTranVan03 <ttoan17112003@gmail.com>
2026-03-14 10:17:38 +0700

    QLSPHAM VA QLDMUC

--- TechShare/obj/Debug/net9.0/TechShare.AssemblyInfo.cs
+++ TechShare/obj/Debug/net9.0/TechShare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechShare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34606b94ef252001f1774085bafb68a5ee04be76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b1b8255b252a33da41b160fa11d4a44542054a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechShare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechShare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
